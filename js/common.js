6093d92f2782c9eac2df712b5da5b59afb3c4dfe
haymitchkr <64484172+haymitchkr@users.noreply.github.com>
2020-06-30 17:06:15 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -9,23 +9,6 @@ $(function(){
 		    return false;
 		});
 	});
-	// img to svg 
-	$('img.img-svg').each(function(){
-	  var $img = $(this);
-	  var imgClass = $img.attr('class');
-	  var imgURL = $img.attr('src');
-	  $.get(imgURL, function(data) {
-	    var $svg = $(data).find('svg');
-	    if(typeof imgClass !== 'undefined') {
-	      $svg = $svg.attr('class', imgClass+' replaced-svg');
-	    }
-	    $svg = $svg.removeAttr('xmlns:a');
-	    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-	      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
-	    }
-	    $img.replaceWith($svg);
-	  }, 'xml');
-	});
 	//slick
 	$('.head-slider').slick({
 		dots: true,
@@ -41,20 +24,70 @@ $(function(){
 		$('.burger').click(function(){
 			$('.burger,.header__nav,.header__logo').toggleClass('active');
 			$('.blur-container').toggleClass('active');
-			$('body').toggleClass('lock');
 		});
 		$(document).mouseup(function (e){
 			if (!$(".nav-list__item,.burger").is(e.target)) {
 				$(".header__nav,.burger,.header__logo").removeClass("active");
-				$('.blur-container').removeClass('active')
-				$('body').removeClass('lock');};
+				$('.blur-container').removeClass('active')};
 		});
 		$(window).scroll(function (){
-			if ($(window).scrollTop()>= 35){
+			if ($(window).scrollTop()>= 40){
 				$('.header__logo').addClass('hidden')
 			}else{
 				$('.header__logo').removeClass('hidden')
 			}
 		});
+	});
+	//latest-project burger
+	$(function(){
+		$('.latest-project__nav-burger').click(function(){
+			$('.latest-project__nav-burger,.latest-project__nav').toggleClass('active');
+		});
+		$(document).mouseup(function (f){
+			if (!$(".latest-project__nav-burger,latest-project__nav").is(f.target)){
+				$('.latest-project__nav-burger,.latest-project__nav').removeClass('active');
+			};
+		});
+	});
+	$(function(){
+		$('.recent-post__slider').slick({
+			slidesToShow: 3,
+			arrows: true,
+			dots: false,
+			adaptiveHeight: true,
+			prevArrow: $('.recent-post__arrow-prev'),
+			nextArrow: $('.recent-post__arrow-next'),
+			responsive: [
+				{
+				breakpoint: 1024,
+				settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,}
+				},
+				{
+				breakpoint: 600,
+				settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1}
+				}
+			]
+		});
+	});
+		// img to svg 
+	$('img.img-svg').each(function(){
+	  var $img = $(this);
+	  var imgClass = $img.attr('class');
+	  var imgURL = $img.attr('src');
+	  $.get(imgURL, function(data) {
+	    var $svg = $(data).find('svg');
+	    if(typeof imgClass !== 'undefined') {
+	      $svg = $svg.attr('class', imgClass+' replaced-svg');
+	    }
+	    $svg = $svg.removeAttr('xmlns:a');
+	    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+	      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+	    }
+	    $img.replaceWith($svg);
+	  }, 'xml');
 	});
 });
